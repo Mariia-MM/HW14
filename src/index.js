098ac12f05a7be6662fc8f5479a5fc06e105d07b
Mariia <mytsko.mariia@gmail.com>
2020-09-07 01:30:27 +0200

    redaction

--- src/index.js
+++ src/index.js
@@ -11,23 +11,16 @@ import myImg from "./HW10/files/img2.jpg";
 console.log(`hello node cursor lalala`);
 console.log(data);
 
-//console.log(HW4.getHW4());
 HW4.getHW4();
-//console.log(HW5.getHW5());
+
 HW5.getHW5();
-//console.log(HW6.getHW6());
+
 HW6.getHW6();
 console.log(HW7.getHW7());
 HW7.getHW7();
 HW9.getHW9();
 function component() {
   const element = document.createElement("div");
-
-  // Lodash, now imported by this script
-  //   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  //   element.classList.add("hello");
-
-  // Add the image to our existing div.
   const myIcon = new Image();
   myIcon.src = myImg;
 
